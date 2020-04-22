b9b836a1147c8482136835ca466ec7b45accd3f9
tripathiritik29 <ritiktripathi29@gmail.com>
2020-04-22 15:19:11 +0530

    Updates

---
+++ precache-manifest.7b808b0faf6b5f1c9d2821c5becd845e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7b872af34b90e7999dd26cd12770d1a",
+    "url": "/chat-app1/index.html"
+  },
+  {
+    "revision": "08f9a06145cd7c7e0f2b",
+    "url": "/chat-app1/static/css/main.e334640f.chunk.css"
+  },
+  {
+    "revision": "05b754681cf495d57065",
+    "url": "/chat-app1/static/js/2.fa459354.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chat-app1/static/js/2.fa459354.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08f9a06145cd7c7e0f2b",
+    "url": "/chat-app1/static/js/main.068e59c9.chunk.js"
+  },
+  {
+    "revision": "a58b72be85c170e26d56",
+    "url": "/chat-app1/static/js/runtime-main.ded67036.js"
+  }
+]);
